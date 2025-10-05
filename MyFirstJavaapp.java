f9baad4923d521430d75d6178a010a99db6c22a4
Azizmohammad-Sahak <azizmohammadsahak@gmail.com>
2025-10-05 11:39:04 +0430

    Add files via upload

---
+++ MyFirstJavaapp.java
@@ -0,0 +1,16 @@
+
+package myfirstjavaapp;
+
+import java.util.Scanner;
+public class MyFirstJavaapp {
+
+    public static void main(String[] args) {
+
+System.out.println("enter the feet");
+double foot=new Scanner(System.in).nextDouble();
+double meter=0.305*foot;
+System.out.println(meter);
+    }
+}
+
+
